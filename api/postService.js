73fccbfd1a431a3048f5d1178294b392a49d6bf7
Geraldo Mateus Reis <reis.geraldomateus@gmail.com>
2017-09-18 03:56:36 -0300

    Metodos para adicionar imagem no servidor

--- api/postService.js
+++ api/postService.js
@@ -30,6 +30,111 @@ function addPost(req, res) {
 
 }
 
+
+var fs = require('fs');
+
+// function to encode file data to base64 encoded string
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
+
+// function to create file from base64 encoded string
+function base64_decode(base64str, file) {
+    // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
+    var bitmap = new Buffer(base64str, 'base64');
+    // write buffer to file
+    fs.writeFileSync(file, bitmap);
+    console.log('******** File created from base64 encoded string ********');
+}
+
+
+function addImage(req, res) {
+
+  var formidable = require('formidable')
+  , http = require('http');
+
+  var form = new formidable.IncomingForm();
+
+  form.parse(req, function(err, fields, files) {
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.write('received upload:\n\n');
+    var image = files.image
+      , image_upload_path_old = image.path
+      , image_upload_path_new = './upload/'
+      , image_upload_name = image.name
+      , image_upload_path_name = image_upload_path_new + image_upload_name;
+
+    if (fs.existsSync(image_upload_path_new)) {
+
+
+      // convert image to base64 encoded string
+      var base64str = base64_encode(image_upload_path_old);
+      console.log(base64str);
+      // convert base64 string back to image
+      base64_decode(base64str, image_upload_path_name);
+
+      /*
+      fs.rename(
+        image_upload_path_old,
+        image_upload_path_name,
+        function (err) {
+          if (err) {
+            console.log('Err: ', err);
+            res.end('Deu merda na hora de mover a imagem!');
+          }
+          var msg = 'Imagem ' + image_upload_name + ' salva em: ' + image_upload_path_new;
+          console.log(msg);
+          res.end(msg);
+        });
+        */
+    } else {
+      fs.mkdir(image_upload_path_new, function (err) {
+        if (err) {
+          console.log('Err: ', err);
+          res.end('Deu merda na hora de criar o diretório!');
+        }
+/*
+        fs.rename(
+          image_upload_path_old,
+          image_upload_path_name,
+          function(err) {
+            var msg = 'Imagem ' + image_upload_name + ' salva em: ' + image_upload_path_new;
+            console.log(msg);
+            res.end(msg);
+          });
+          */
+      });
+    }
+  });
+
+
+
+
+  const id = "d0b265f12f3f7c14926c721354fdb499";
+
+  //console.log(req);
+
+  //res.status(201).end(JSON.stringify("OK"));
+
+/*
+  connection.insert(newPost, function(err, body, header) {
+    if (err) {
+      res.status(400).end();
+      return console.log('[posts.insert] ', err.message);
+    }
+
+    newPost._id = body.id;
+    delete newPost.time;
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(201).end(JSON.stringify(newPost));
+  });
+*/
+}
+
 // GET /api/posts/find/:id
 function findPostById(req, res) {
   const postId = req.params.id;
@@ -100,4 +205,4 @@ function findAllPosts(req, res) {
 
 }
 
-module.exports = { addPost, findPostById, findAllPosts }
+module.exports = { addPost, findPostById, findAllPosts, addImage }
